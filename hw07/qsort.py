776d4bb5cb5610b50f33481666cd5d94cb0753fc
captiosus <royxu652@yahoo.com>
2016-04-11 11:56:30 -0400

    update folders

--- hw07/qsort.py
+++
@@ -1,5 +0,0 @@
-def qsort(list):
-    return [] if not list else qsort([x for x in list if x < list[0]])+[x for x in list if x == list[0]]+qsort([x for x in list if x > list[0]])
-
-unallocated = [2, 6, 1, 3, 8, 13, 213, 4, 6, 8, 4, 21, 3, 7, 9, 0]
-print qsort(unallocated)
