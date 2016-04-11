2abf7ceca17e550f9df74f1dba363ad71df2ed6e
captiosus <royxu652@yahoo.com>
2016-04-11 11:52:54 -0400

    fix

--- hw09/hw09.py
+++ hw09/hw09.py
@@ -1,18 +1,21 @@
 import time
 
+
 def funcArgs(f):
-    return lambda *arg: f.func_name + ": " + str(*arg) + "\n" + f(*arg)
+    return lambda *arg: f.func_name + ": " + str(arg) + "\n" + f(*arg)
+
 
 def evalTime(f):
     start = time.time()
-    return lambda *arg: f(*arg) + "\n" + str(time.time()-start)
+    return lambda *arg: f(*arg) + "\n" + str(time.time() - start)
+
 
 @evalTime
 @funcArgs
-def hello(string):
+def hello(string, string2):
     for x in range(6):
         for y in range(6000000):
-            test = x*y
-    return "hello world " + string
+            test = x * y
+    return "hello world " + string + string2
 
-print hello("hi")
+print hello("hi", "bye")
